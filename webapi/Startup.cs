14d618bad7d79f6d77c011ee7ce33c87ee725d63
Lakshay Sharma <lakshay35@gmail.com>
2020-06-30 17:27:03 -0400

    added options middleware

--- webapi/Startup.cs
+++ webapi/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -11,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using webapi.business;
+using webapi.middleware;
 using webapi.models.interfaces;
 
 namespace webapi
@@ -34,6 +36,7 @@ namespace webapi
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+      app.UseOptionsHandlerMiddleware();
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
@@ -44,6 +47,8 @@ namespace webapi
       app.UseRouting();
 
       app.UseAuthorization();
+
+      app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
       app.UseEndpoints(endpoints =>
       {
